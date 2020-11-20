635e689c368dea18efd2c1952630ab5702113233
HannahNCI <44295285+HannahNCI@users.noreply.github.com>
2020-11-20 14:09:27 +0000

    Add files via upload

---
+++ daysofweek.sql
@@ -0,0 +1,14 @@
+CREATE VIEW daysofweek (day) AS
+    SELECT 'Monday' 
+    UNION 
+    SELECT 'Tuesday'
+    UNION 
+    SELECT 'Wednesday'
+    UNION 
+    SELECT 'Thursday'
+    UNION 
+    SELECT 'Friday'
+    UNION 
+    SELECT 'Saturday'
+    UNION 
+    SELECT 'Sunday';
