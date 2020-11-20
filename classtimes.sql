df0246e3f35c0f15ac4c818a9dd66af92d62c85a
HannahNCI <44295285+HannahNCI@users.noreply.github.com>
2020-11-20 14:08:52 +0000

    Add files via upload

---
+++ classtimes.sql
@@ -0,0 +1,28 @@
+CREATE VIEW classtimes (time) AS
+	SELECT '6:00' 
+    UNION 
+	SELECT '7:00' 
+    UNION 
+	SELECT '8:00' 
+    UNION 
+    SELECT '9:00' 
+    UNION 
+    SELECT '10:00'
+    UNION 
+    SELECT '11:00'
+    UNION 
+    SELECT '12:00'
+    UNION 
+    SELECT '13:00'
+    UNION 
+    SELECT '14:00'
+    UNION 
+    SELECT '16:00'
+    UNION 
+	SELECT '18:00'
+    UNION 
+	SELECT '19:00'
+    UNION 
+	SELECT '20:00'
+    UNION 
+	SELECT '21:00';
